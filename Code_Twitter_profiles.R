405a14661bd09275c34c1168d54c10e0a8d5cab9
toupinr <67013546+toupinr@users.noreply.github.com>
2020-06-16 11:37:55 -0400

    Add files via upload

---
+++ Code_Twitter_profiles.R
@@ -0,0 +1,194 @@
+## =================
+# Rémi code for users profiles
+# created: 25 Nov 2019
+# updated: 15 June 2020
+
+library(dplyr)
+library(reshape2)
+library(stringr)
+library(ggplot2)
+library(vegan)
+library(readxl)
+library(quanteda)
+library(tidyr)
+library(tidytext)
+library(tidyverse)
+library(UpSetR)
+
+## =================
+# load profiles info
+## =================
+setwd(" ")
+users <- read_excel(" ", sheet=" ", col_names = TRUE)
+head(users)
+
+
+## =================
+# basic string cleaning
+## =================
+
+#filter by ?
+#users <- users %>%
+#  filter(Year == 2016)
+
+#remove duplicates
+users <- users[!duplicated(users$Author_id_on_Source), ]
+
+#remove capitals
+users$description <- tolower(users$Author_Description)
+
+#remove urls
+users$description <- gsub(" ?(f|ht)(tp)(s?)(://)(.*)[.|/](.*)", "", users$description)
+users$description
+
+#remove emojis
+users$description <- gsub("[^\x01-\x7F]", "", users$description)
+users$description
+
+#remove numbers
+users$description <- gsub("[0-9]+", "", users$description)
+users$description
+
+#remove lists with / and replace with " " (e.g., wife/phd/friend)
+users$description <- gsub("\\/", " ", users$description)
+
+# forward slash -- escape with a double backslash
+sub("\\/", " ", "Peace/Love")
+#[1] "Peace Love"
+
+length(unique(users$description))
+
+#remove punctuations
+punct <- '[]\\?!\"\'$%&(){}+*/:;,._`|~\\[<=>\\^-]'
+users$description <- gsub(punct, "", users$description)
+users$description
+
+#remove NULL
+users <- users %>% 
+  filter(!grepl('NULL', Author_Description))
+
+users <- users %>% 
+  filter(!is.na(description))
+
+users
+users$description
+
+## =================
+#code to faculty
+## =================
+#lectur*, prof* 
+
+sci <- "\\blectur(er|ing)\\b|\\bprof\\b|\\bprofess(o|eu)re?\\b|\\b(ph|m)\\s?(d|sc)i?\\b|\\bpost\\s?doc(\\w+)?\\b|\\bdoctora(l|nt)\\b|\\bstudent\\b|\\bresearch(er|ing|\\s(fellow|assistant|associate|leader|director))\\b|\\bi\\s(research|study)\\b|\\bscient(i(st|fique))?\\b|\\bchair\\b|\\bassociate\\sdirector\\b|\\b((micro)?bio|(e|é)co|pala?eonto|viro|onco|hema|epidemio|physio|archa?eo|anthropo|psycho|patho|endocrino|nephro|limno|socio|neuro|geo|physic|botan|entomo|hydro|genetic|chem|ethic)(log)?(u|ist|ien(ne)?)e?\\b|\\b(\\w+)?grad(\\w+)?\\b|\\borcid\\b|\\b(bachelor|master)\\sdegree\\b|\\bdean\\b|\\bhistori(e|a)n(ne)?\\b|\\bfaculty\\b|\\bscholar\\b|\\b(g(e|é)|oc(e|é)an)ographer?\\b|\\b#sci(comm|art|engage)\\b|\\bprgrm\\b|\\bacad(é|e)mi(c|que)\\b|\\bétudiante?\\b|\\bchercheure?\\b|\\borcid\\b"
+testsci <- c("lecturer in marine","university professor",
+             "journal of the","university of X biology phd student",
+             "project","professora", "ph d", "post doc", "postdoc", "doctoral", "doctorant", "researcher", "researchers", "researching", "i research", "research associate", "research director", "fellow", "research project", "i study", "scientist", "lecturing", "scientifique", "associate director", "biologist", "écologiste", "ecologist", "microbiologiste", "archaeologist", "sociologist", "sociologue", "physicist", "physicien", "physicienne", "entomologiste", "botaniste", "hydrologist", "physicists", "graduate", "postgrad", "master degree", "faculty", "dean", "msc", "m sc", "postdoctoral", "scient", "historienne", "géographe", "#scicomm", "msci", "académique", "professeur", "étudiante", "chercheur")
+
+grepl(sci,testsci)
+
+## =================
+#code to comm
+## =================
+#journalist*, reporter*
+
+comm <- "\\bwr(i|o)t(e(r?|s?)|ing)\\b|\\baut(hor|eur)\\b|\\bjourn(o|aliste?)\\b|\\bblog(ger|ging|s?\\s(for|on))?\\b|\\bcolumnist\\b|\\bcommunicat((o|eu)r|ion|ing)\\b|\\bfreelance.?\\swri.+\\b|\\b(repor|pr(e|é)sen|(e|é)di|(e|é)duca|contribu|produc|teach|storytell|commentat|broadcast|youtub|chroniq|speak)(t?a?t?((eu?|o)r|rice))\\b|\\bpodcast(er)?\\b|\\bcorrespond(e|a)nt\\b|\\bwebsite\\b|\\bfilms?\\b|\\bshow\\shost\\b|\\bdocu(mentary)?\\b|\\bsharing\\sscience\\b|\\bcameraman\\b|\\béditorialiste\\b|\\b@carbon\\sbrief\\b|\\bcurated\\sby\\b"
+testcomm <- c("auteur", "writer", "co author", "journaliste", "journal", "blogs", "blog", "blogs for", "freelance writing", "freelance worker", "reporter", "podcaster", "write", "correspondent", "website", "educator", "producer", "éditrice", "présentatrice", "film", "show host", "communication", "docu", "youtuber", "writes", "writers", "@carbon brief")
+
+grepl(comm,testcomm)
+
+## =================
+#code to organization
+## =================
+
+organ <- "\\b(universit(y|é)|institute?|societ(y|é)|library|lab(o?(rato(ry|ire))?)|college|community|organi((z|s)ation|sme)|association|school|d(e|é)p(t|arte?ment)|program(me)?|faculté|cent(er|re)|team|groupe?|advocates|leaders|experts|branch|parternship|network|compag?n(y|i)e?|alliance|initiative|channel|specialists|hub|agency|office|bureau|charity|movement|founded|nonprofit|llc|club|courses|campus|station|collaboration|project|designs|inc|grouping|forum|citizens|consultants|committee|offici(a|eu?)(l|x)|toolkit)\\b|\\bjoin\\s(us|the)\\b|\\bpromotes\\b|\\bour\\sgoal\\b|\\bwe\\s((sh)?a?re|want|campaign|deliver)\\b|\\blive\\sevents\\b|\\b(tweets|managed)\\sby\\b|\\bmail\\sat\\b|\\baims\\sto\\b|\\bcoming\\ssoon\\b"
+testorgan <- c("université", "society", "college", "département", "labo", "lab", "laboratoire", "centers", "organisme", "tweets by", "we share", "we are", "officiel")
+
+grepl(organ,testorgan)
+
+## =================
+#code to bots
+## =================
+
+bot <- "\\b(ro)?bot\\b|\\b(paper|publication|lit|preprint|article|peer-review|journal|news|data)\\s?feed\\b|\\brss\\b|\\bnew\\s(submissions?|papers?)\\b|\\b(latest|new)\\spublications?\\b|\\b(a|bio)rxiv\\b|\\bpubmed\\b|\\bpapers?\\s(auto|stream|tweet|updates?|links?|alerts?)|\\bpublications?\\salerts?\\b|\\bdaily\\s(updates?|compilations?)\\b|\\bcompilations?\\b|retweet(s|ed)?\\s(from|any)\\b"
+testbot <- c("robot", "news feed", "datafeed", "new submissions", "paper alerts", "daily updates", "retweets from")
+
+grepl(bot,testbot)
+
+## =================
+#code to professional
+## =================
+
+pro <- "\\b(physi|pediatri)cian\\b|\\bdoct(eu|o)re?\\b|\\bprofession(a|e)l(le)?\\b|\\bnurse\\b|\\bnutritionist\\b|\\bc(e|t)o\\b|\\bvet(erinarian)?\\b|\\bconsult(ing|ante?)\\b|\\bofficer\\b|\\banalyste?\\b|\\bfarmer\\b|\\btrustee\\b|\\bdir(ect(o|eu)?r(ice)?)?\\b|\\b(é|e)conomiste?\\b|\\bentrepreneur\\b|\\bstewardship\\b|\\bstrat(e|è)g(e|ist)\\b|\\bwork(ing)?\\s(at|for)\\b|\\b(e|i)ng(ineer|énieure?)\\b|\\btranslator\\b|\\bltd\\b|\\bagriculteur\\b|\\bmanager\\b|\\blawyer\\b|\\bplanner\\b|\\bd(é|e)velop(e|eu)r\\b|\\borganiser\\b|\\badvis(o|e)r\\b|\\battorney\\b|\\bcounsel\\b|\\bsecretary\\b|\\bself\\semployed\\b|\\bfisherman\\b|\\b(co\\s)?fou?nd(at)?eu?r(ice)?\\b|\\bconservationist\\b|\\bdesigner\\b|\\bhead\\sof\\b|\\binvestor\\b|\\bprincipal\\sat\\b|\\bcoord(o|i)nat(ing|(eu|o)?r(ice)?)\\b|\\bemployer\\b|\\bpaysan\\b|\\pr(é|e)sidente?\\b|\\bbusiness\\s(owner|development)\\b?|\\bspecialist\\b|\\bhandyman\\b|\\bsolicitor\\b|\\bsustainable\\sfinance\\b|\\bweb\\sdev\\b|\\bday\\sjob\\b|\\bchairman\\b|\\blibrarian\\b|\\bcoach\\b|\\bénergéticien\\b|\\bvp\\b|\\bexecutive\\b|\\barchitect\\b|\\bexperienced\\b"
+testpro <- c("physician", "doctor", "public health professional", "directeur", "directrice", "analyste", "consultant", "ceo", "self-employed")
+
+grepl(pro,testpro)
+
+## =================
+#code to personal
+## =================
+
+pers <- "\\bwife\\b|\\bmale\\b|\\b(grand)?(m|p|f|d)(o|u|a)(d|m(my)?|ther)?\\b|\\bphotographer?\\b|\\bwhite\\b|\\bfood\\b|\\bgeek\\b|\\bhusband\\b|\\bmusic\\b|\\brunn(er|ing)\\b|\\b(m|p)ère\\b|\\bmarr?i(ed|ée?)?\\b|\\byoga\\b|\\(dream|ad?ventur)i?(e|è)?re?\\b|\\bliterature\\b|\\bcycli(ng|st)e?\\b|\\bvélo\\b|\\dan(c|s)eu?r?\\b|\\bentho?usiaste?\\b|\\bfilms\\b|\\blov(e(s|r)?|ing)\\b|\\breligi(ous|eu(x|se))\\b|\\bsurfeu?(r|se)\\b|\\travel(ler)?\\b|\\bvoyage(s|eu(r|s)e?)?\\b|\\bvol(u|o)nt(ai|ee)re?\\b|\\byoung\\b|\\bgardener\\b|\\bhockey\\b|\\bdivorc(é|e)(d|e)?\\b|\\bdog\\b|\\bcat\\b|\\bhobby(ist)?\\b|\\bartiste?\\b|\\br(e|é)sidente?\\b|\\bbirder\\b|\\bknit(ter|ting)?\\b|\\bfamily\\b|\\peinture\\b|\\bjazz\\b|\\bcomedy\\b|\\bgolf\\b|\\b(v|)wine?\\b|\\bcoffee\\b|\\btea\\b|\\bscuba\\b|\\bswimming\\b|\\bbik(es?|ing)\\b|\\bclimb(er|ing)\\b|\\bnerd\\b|\\bsport\\b|\\bsailing\\b|\\bhumanitarian\\b|\\bgame(s|r)\\b|\\bcamping\\b|\\bfishing\\b|\\bfencer\\b|\\bhiker\\b|\\bretired\\b|\\bbibliophile\\b|\\b(papa|maman)\\b|\\badmirer\\b|\\bjunkie\\b|\\bpassion((at|é)e?)?s?\\b|\\bbeer\\b|\\bphotography\\b|\\buncle\\b|\\btinkerer\\b|\\bplayer\\b|\\branter\\b|\\bcontemplating\\slife\\b|\\bveggies\\b|\\bski(ing|er)?\\b|\\bfoodie\\b|\\binterests\\b|\\bfreethinker\\b|\\bsurf\\b|\\bfootball\\b|\\bamateur\\b"
+testpers <- c("mom", "grandma", "mother", "grandmother", "grandfather", "dad", "grandpa", "père")
+
+grepl(pers,testpers)
+
+## =================
+#code to political
+## =================
+
+poli <- "\\b#resist\\b|\\badvoca(t(e|ing)|cy)\\b|\\bactivis(m|t)e?\\b|\\bcampaign(er¡ing)\\b|\\benvironmentalist\\b|\\blefti(st|sh)\\b|\\bnationalist\\b|\\bliberal\\b|\\bconservati(sm|ve)\\b|\\bmilitante?\\b|\\bfight(ing)?\\b|\\b#?climat\\s?eaction\\b|\\binequality\\b|\\b(human|women\\s)\\srights\\b|\\bsocial\\sjustice\\b|\\b#?degrowth\\b|\\b#lowtech\\b|\\b#?progressi(ve|ste?)\\b|\\bpolicy\\b|\\b@piratepartyau\\b|\\b#berner\\b|\\#?fossil\\s?fuels?\\b|\\bpachamama\\b|\\bcouncillor\\b|\\bstand\\swith\\b|\\bdecoloniz(e|ing)\\b|\\btransition\\b|\\bgreens\\smember\\b|\\bopposed\\b|\\bgreenie\\b|\\bfeminis(m|t)\\b|\\btree\\s?hugger\\b|\\b#?divest?\\b|\\b(#go)?vegan\\b|\\bbernie\\b|\\bconcerned\\b|\\b#?france\\s?insoumise\\b|\\bemergency\\b|\\balternative\\b|\\bagent\\sof\\schange\\b|\\bplanet\\b|\\b\\w?solar\\s?warrior\\b|\\b#?the\\sresistance\\b|\\beco\\swarrior\\b|\\bcommunity\\sleader\\b|\\bdéclin\\b|\\bsenator\\b|\\bstop\\sclimate\\schange\\b|\\b#?save\\s?the\\s?arctic\\b|\\bgender\\sequity\\b|\\b#earthourhome\\b|\\bmayor\\b|\\brefugee\\sjustice\\b|\\b#?climate\\s?hawk\\b|\\b#animallover\\b|\\benvironmental\\sissues\\b|\\b#standupforscience\\b|\\bstop\\b|\\bresilience\\b|\\bnonuke\\b|\\bcounsellor\\b|\\bcalling\\sfor\\b|\\bparliament\\b|\\bcllr\\b|\\b#renewables\\b|\\bblackfish\\b|\\b#?act\\s?on\\s?climate\\b|\\bban\\sfracking\\b|\\b#?climate\\s?justice\\b|\\bresisting\\b|\\bthreatened\\b|\\b#feelthebern\\b|\\bdeniers\\b|\\b#maga\\b|\\b#shutdowntanks\\b|\\bclimate\\scrisis\\b|\\b#?carbon\\s?neutrality\\b"
+testpoli <- c("fossil fuels", "france", "solar warrior", "solar", "#solarwarrior", "#govegan", "save the arctic", "#savethearctic")
+
+grepl(poli,testpoli)
+
+## =================
+#code to political
+## =================
+
+pub <- "\\bjournals?\\b|\\bpublish(er|ing)s?\\b|\\b\\s?press\\b|\\btaylor\\s+francis\\b|\\belsevier\\b|\\bwiley\\b|\\broutledge\\b|\\bspringer\\b|\\bsage\\b|\\bjrnl\\b|\\bissn\\b"
+testpub <- c("journal", "publishing", "publishers", "oxford press", "taylor francis", "taylor  francis")
+
+grepl(pub,testpub)
+
+## =================
+#integration in lists
+## =================
+
+users$sci <- ifelse(grepl(sci,users$description) , 1, 0)
+users$comm <- ifelse(grepl(comm,users$description) , 1, 0)
+users$organ <- ifelse(grepl(organ,users$description) , 1, 0)
+users$bot <- ifelse(grepl(bot,users$description) , 1, 0)
+users$pro <- ifelse(grepl(pro,users$description) , 1, 0)
+users$pers <- ifelse(grepl(pers,users$description) , 1, 0)
+users$poli <- ifelse(grepl(poli,users$description) , 1, 0)
+users$pub <- ifelse(grepl(pub,users$description) , 1, 0)
+
+science <- filter(users, users$sci == 1)
+communication <- filter(users, users$comm == 1)
+organization <- filter(users, users$organ == 1)
+bots <- filter(users, users$bot == 1)
+professional <- filter(users, users$pro == 1)
+personal <- filter(users, users$pers == 1)
+political <- filter(users, users$poli == 1)
+publisher <- filter(users, users$pub == 1)
+notassigned <- filter(users, users$sci == 0, users$comm == 0, users$organ == 0, users$bot == 0, users$pro == 0, users$pers == 0, users$poli == 0, users$pub == 0)
+
+sciencelist <- as.list(science$Author_id_on_Source)
+commlist <- as.list(communication$Author_id_on_Source)
+orglist <- as.list(organization$Author_id_on_Source)
+botlist <- as.list(bots$Author_id_on_Source)
+prolist <- as.list(professional$Author_id_on_Source)
+persolist <- as.list(personal$Author_id_on_Source)
+pollist <- as.list(political$Author_id_on_Source)
+publist <- as.list(publisher$Author_id_on_Source)
+notlist <- as.list(notassigned$Author_id_on_Source)
+
+fullist <- list(Science = c(sciencelist), Communication = c(commlist), Organization = c(orglist), Professional = c(prolist), Politic = c(pollist), Personal = c(persolist), Bots = c(botlist), Publishers = c(publist))
+
+# create list, profiles without keywords
+# , ND = c(notlist)
+
+#=======================
+# plot data with upsetr
+#=======================
+upset(fromList(fullist), nsets = 8, nintersects = 30, number.angles = 30, point.size = 3, mainbar.y.label = "Intersections", sets.x.label = "Ampleur des catégories", order.by = "freq", text.scale = c(1.3, 1.3, 1.3, 1.3, 1.3, 1.3))
+
